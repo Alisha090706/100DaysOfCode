b111abb0310bb610af011e303d5e0cf543e07934
Alisha Kapoor <alishakapoor0907@gmail.com>
2025-03-27 12:56:09 +0530

    Added Day13 folder

---
+++ Day13/Debugging.py
@@ -0,0 +1,55 @@
+#Code1
+# def odd_or_even(number):
+#     if number % 2 = 0:
+#         return "This is an even number."
+#     else:
+#         return "This is an odd number."
+def odd_or_even(number):
+    if number % 2 == 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."
+#Code2
+# def is_leap(year):
+#     if year % 4 == 0:
+#         if year % 100 == 0:
+#             if year % 4000 == 0:
+#                 return True
+#             else:
+#                 return False
+#         else:
+#             return True
+#     else:
+#         return False
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+#Code3: Print numbers from 1 upto target. When the number is divisible by 3, instead of printing number, print "Fizz". For a number divisible by 5, print "Buzz". For a number divisible by both 3 and 5 i.e. 15, print "FizzBuzz".
+# def fizz_buzz(target):
+#     for number in range(1, target + 1):
+#         if number % 3 == 0 or number % 5 == 0:
+#             print("FizzBuzz")
+#         if number % 3 == 0:
+#             print("Fizz")
+#         if number % 5 == 0:
+#             print("Buzz")
+#         else:
+#             print([number])
+def fizz_buzz(target):
+    for number in range(1, target + 1):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
